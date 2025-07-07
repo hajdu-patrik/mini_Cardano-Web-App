4c672beb531371b1956c1271d8c611629f1aadb5
Hajdu Patrik Zsolt <hajdu.patrik1023@gmail.com>
2025-07-07 21:50:00 +0200

    Fix: removed unused SpeedInsights import to resolve Vercel build error

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import WalletConnector from './components/WalletConnector'
-import './App.css'
+import WalletConnector from './components/WalletConnector';
+import './App.css';
 
 function App() {
   return (
     <>
       <h1>Welcome on the Cardano Wallet Connector App</h1>
       <WalletConnector />
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
